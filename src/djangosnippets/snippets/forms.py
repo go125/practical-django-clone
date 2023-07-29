04468d048ae230606b2aeb6b9cef6e866513dc16
go125 <34290064+go125@users.noreply.github.com>
2023-07-29 19:20:04 +0900

    fix: タグ編集ができるようにformsも修正

--- src/djangosnippets/snippets/forms.py
+++ src/djangosnippets/snippets/forms.py
@@ -4,4 +4,4 @@ from snippets.models import Snippet
 class SnippetForm(forms.ModelForm):
     class Meta:
         model=Snippet
-        fields=('title','code','description')
+        fields=('title','code','description','tag')
