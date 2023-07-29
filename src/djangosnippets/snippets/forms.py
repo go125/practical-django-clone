2b9fc5094c693224a1b721ec13bfecc6556449b1
go125 <34290064+go125@users.noreply.github.com>
2023-07-29 19:25:11 +0900

    add: label

--- src/djangosnippets/snippets/forms.py
+++ src/djangosnippets/snippets/forms.py
@@ -3,6 +3,7 @@ from snippets.models import Snippet, Tag
 
 class SnippetForm(forms.ModelForm):
     Tag_Select = forms.ModelMultipleChoiceField(
+        label="タグを選択してください",
         queryset=Tag.objects,
         widget=forms.CheckboxSelectMultiple)  
     class Meta:
